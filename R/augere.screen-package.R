3f626fc3478ef73e8c6042597b421c2de2927e51
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-01-09 15:26:50 +1100

    Pre-emptively implement minor fixes from BioC review.
    Also added a bunch of biocViews.

---
+++ R/augere.screen-package.R
@@ -0,0 +1,9 @@
+#' augere.screen
+#'
+#' Implement functional screen analyses in the augere framework.
+#' The \code{\link{runScreen}} function tests for differential barcode abundance with voom,
+#' also generating a self-contained Rmarkdown report to reproduce each analysis.
+#'
+#' @name augere.screen-package
+NULL
+
