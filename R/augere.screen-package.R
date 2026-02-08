88a8cdb2f9b7a9bab2cf30d80be39b742d166140
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-02-08 22:43:28 +1100

    Auto-add details to the package manpage.

--- R/augere.screen-package.R
+++ R/augere.screen-package.R
@@ -1,9 +1,2 @@
-#' augere.screen
-#'
-#' Implement functional screen analyses in the augere framework.
-#' The \code{\link{runScreen}} function tests for differential barcode abundance with voom,
-#' also generating a self-contained Rmarkdown report to reproduce each analysis.
-#'
 #' @name augere.screen-package
-NULL
-
+"_PACKAGE"
